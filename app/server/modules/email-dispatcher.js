242de978e0ad61415374731c68031939a022fcb3
UEDeveloper <66185256+MidasLeaks@users.noreply.github.com>
2021-02-11 23:09:51 +0000

    Update email-dispatcher.js

--- app/server/modules/email-dispatcher.js
+++ app/server/modules/email-dispatcher.js
@@ -5,15 +5,15 @@ module.exports = EM;
 EM.server = require("emailjs/email").server.connect(
 {
 	host 	    : process.env.NL_EMAIL_HOST || 'smtp.gmail.com',
-	user 	    : process.env.NL_EMAIL_USER || 'your-email-address@gmail.com',
-	password    : process.env.NL_EMAIL_PASS || '1234',
+	user 	    : process.env.NL_EMAIL_USER || 'fivecheatscom@gmail.com',
+	password    : process.env.NL_EMAIL_PASS || '52Boghall',
 	ssl		    : true
 });
 
 EM.dispatchResetPasswordLink = function(account, callback)
 {
 	EM.server.send({
-		from         : process.env.NL_EMAIL_FROM || 'Node Login <do-not-reply@gmail.com>',
+		from         : process.env.NL_EMAIL_FROM || 'Five Cheats <do-not-reply@gmail.com>',
 		to           : account.email,
 		subject      : 'Password Reset',
 		text         : 'something went wrong... :(',
@@ -23,7 +23,7 @@ EM.dispatchResetPasswordLink = function(account, callback)
 
 EM.composeEmail = function(o)
 {
-	let baseurl = process.env.NL_SITE_URL || 'http://localhost:3000';
+	let baseurl = process.env.NL_SITE_URL || 'http://fivecheats.com:3000';
 	var html = "<html><body>";
 		html += "Hi "+o.name+",<br><br>";
 		html += "Your username is <b>"+o.user+"</b><br><br>";
